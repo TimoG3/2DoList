21a1932e9563de1401fde0ad3b782391dfec5417
TimoG3 <148169033+TimoG3@users.noreply.github.com>
2023-11-24 18:09:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener('load', () => {
+window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
